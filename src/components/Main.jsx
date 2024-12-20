2238602316337fbe4acf819eaac0ff141920d29d
Saakshi Kobarne <saakshik15@gmail.com>
2024-12-20 21:16:17 +0530

    Initialize and create frontend

---
+++ src/components/Main.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Home from './Home';
+import About from './About';
+import Table from './Table';
+import Stats from './Stats';
+import ScrollToTop from './ScrollToTop';
+
+const Main = () => {
+  return (
+    <Router>
+      <ScrollToTop/>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/data" element={<Table />} />
+            <Route path="/analytics" element={<Stats />} />
+          </Routes>
+        </div>
+
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default Main;
